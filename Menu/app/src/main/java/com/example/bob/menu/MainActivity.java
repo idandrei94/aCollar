bd07601bb04f18320fb333e8f7ddf7b9a9d3114a
lamiita <lamiita0205@gmail.com>
2015-11-23 12:58:55 +0100

    start button

--- Menu/app/src/main/java/com/example/bob/menu/MainActivity.java
+++ Menu/app/src/main/java/com/example/bob/menu/MainActivity.java
@@ -53,7 +53,7 @@ public class MainActivity extends Activity implements WifiP2pManager.ActionListe
             }
         });
 
-        connect = (Button) findViewById(R.id.Start);
+        connect = (Button) findViewById(R.id.start);
         connect.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
